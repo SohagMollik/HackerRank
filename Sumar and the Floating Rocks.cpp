14b5c315347e2328f5a1d0473e1a491bd5d65bb1
SohagMollik <sohagmollik871@gmail.com>
2021-05-22 02:00:03 +0600

    Add files via upload

---
+++ Sumar and the Floating Rocks.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ll    long long
+#define pb    push_back
+#define ull   unsigned long long
+#define f     first
+#define s     second
+#define mod   1000000007
+#define pi    acos(-1.0)
+#define fast  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+        int x=abs(x1-x2);
+        int y=abs(y1-y2);
+        int m=__gcd(min(x,y),max(x,y));
+        cout<<m-1<<endl;
+    }
+
+    return 0;
+}
